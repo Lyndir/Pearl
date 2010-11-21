fc3fbb36a9ba12a9e143e9efa9c0ffed099213a9
Maarten Billemont <lhunath@gmail.com>
2010-11-21 12:30:08 +0100

    Forgot to add.

---
+++ iLibs/UIKit/RootViewController.m
@@ -0,0 +1,87 @@
+    //
+//  RootViewController.m
+//  Gorillas
+//
+//  Created by Maarten Billemont on 12/11/10.
+//  Copyright 2010 lhunath (Maarten Billemont). All rights reserved.
+//
+
+#import "RootViewController.h"
+
+
+@implementation RootViewController
+@synthesize supportedIterfaceOrientations = _supportedIterfaceOrientations;
+
+- (id)init {
+    
+    if (!(self = [super init]))
+        return self;
+    
+    self.supportedIterfaceOrientations = [NSMutableArray array];
+    [self supportInterfaceOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    
+    return self;
+}
+
+- (id)initWithView:(UIView *)aView {
+    
+    if (!(self = [super init]))
+        return self;
+
+    self.view = aView;
+    
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    
+    if (!(self = [super initWithCoder:aDecoder]))
+        return self;
+    
+    self.supportedIterfaceOrientations = [NSMutableArray array];
+    [self supportInterfaceOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    
+    return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    
+    if (!(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+        return self;
+    
+    self.supportedIterfaceOrientations = [NSMutableArray array];
+    [self supportInterfaceOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    
+    return self;
+}
+
+- (BOOL)isInterfaceOrientationSupported:(UIInterfaceOrientation)interfaceOrientation {
+    
+    for (NSNumber *supportedInterfaceOrientation in self.supportedIterfaceOrientations)
+        if ([supportedInterfaceOrientation unsignedIntValue] == interfaceOrientation)
+            return YES;
+    
+    return NO;
+}
+
+- (void)supportInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    
+    if (![self isInterfaceOrientationSupported:interfaceOrientation])
+        [self.supportedIterfaceOrientations addObject:[NSNumber numberWithUnsignedInt:interfaceOrientation]];
+}
+
+- (void)rejectInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    
+    for (NSNumber *supportedInterfaceOrientation in self.supportedIterfaceOrientations)
+        if ([supportedInterfaceOrientation unsignedIntValue] == interfaceOrientation) {
+            [self.supportedIterfaceOrientations removeObject:supportedInterfaceOrientation];
+            return;
+        }
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+
+    return [self isInterfaceOrientationSupported:interfaceOrientation];
+}
+
+@end

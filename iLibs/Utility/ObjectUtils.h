e9114d3909230a2e064d44a4bcf049889ba02f39
Maarten Billemont <lhunath@gmail.com>
2010-11-25 11:59:36 +0100

    Fixes to ArrayTBC and nil macros in ObjectUtils
    [FIXED]     ArrayTBC's handling of nil values.
    [ADDED]     ObjectUtils: nil <-> NSNull macros.

--- iLibs/Utility/ObjectUtils.h
+++ iLibs/Utility/ObjectUtils.h
@@ -10,11 +10,16 @@
 
 #define ListInto(__array, __firstObject)                                                \
             va_list __list;                                                             \
-            va_start(__list, __firstObject);                                              \
+            va_start(__list, __firstObject);                                            \
             for (id __object = __firstObject; __object; __object = va_arg(__list, id))  \
                 [__array addObject:__object];                                           \
             va_end(__list);
 
+#define NilToNSNull(__object)                                                           \
+            (__object == nil? (id)[NSNull null]: __object)
+
+#define NSNullToNil(__object)                                                           \
+            (__object == (id)[NSNull null]? nil: __object)
 
 @interface ObjectUtils : NSObject {
 

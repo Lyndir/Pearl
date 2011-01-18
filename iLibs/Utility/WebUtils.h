d8b748d259b6b58cec6b90dd2f7542b941e42648
Maarten Billemont <lhunath@gmail.com>
2011-01-18 18:00:56 +0100

    Web handling utilities.
    [ADDED]     Utilities for URL encoding.

---
+++ iLibs/Utility/WebUtils.h
@@ -0,0 +1,19 @@
+//
+//  WebUtils.h
+//  iLibs
+//
+//  Created by Maarten Billemont on 18/01/11.
+//  Copyright 2011 lhunath (Maarten Billemont). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WebUtils : NSObject {
+@private
+
+}
+
++ (NSString *)urlEncode:(NSString *)value;
+
+@end

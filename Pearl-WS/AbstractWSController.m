Lyndir/Pearl

commit 0387bfc2252e1287889c8bd07a3449f517f2234f

Turn off WS logging by default, can become quite laggy on XCode

Pearl-WS/AbstractWSController.m
@@ -162,7 +162,7 @@
         if (request.error)
             err(@"Failed from: %@, error: %@", request.url, request.error);
         else
-            inf(@"In from: %@, data:\n%@", request.url, [[[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding] autorelease]);
+//            inf(@"In from: %@, data:\n%@", request.url, [[[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding] autorelease]);
         
         if ([self isSynchronous])
             completion(responseData);

commit cdc15e971878a41f102b7162bcbe20eee4064d2d

Add missing methods for trace logging + fix regression.
[ADDED]     Convenience methods for logging at TRACE level.
[FIXED]     Bug introduced by 0387bfc2.

Pearl-WS/AbstractWSController.m
@@ -73,7 +73,7 @@
 - (ASIHTTPRequest *)requestWithDictionary:(NSDictionary *)parameters method:(WSRequestMethod)method
                                completion:(void (^)(NSData *responseData))completion {
     
-    inf(@"Out to %@, method: %d:\n%@", [self serverURL], method, parameters);
+    dbg(@"Out to %@, method: %d:\n%@", [self serverURL], method, parameters);
     ASIHTTPRequest *request = nil;
     NSData *(^loadRequest)(void) = nil;
     
@@ -155,14 +155,14 @@
     dispatch_block_t handleRequest = ^{
         NSData *responseData = loadRequest();
         if ([request isCancelled]) {
-            inf(@"Cancelled: %@", request.url);
+            dbg(@"Cancelled: %@", request.url);
             completion(nil);
         }
         
         if (request.error)
             err(@"Failed from: %@, error: %@", request.url, request.error);
         else
-//            inf(@"In from: %@, data:\n%@", request.url, [[[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding] autorelease]);
+            trc(@"In from: %@, data:\n%@", request.url, [[[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding] autorelease]);
         
         if ([self isSynchronous])
             completion(responseData);

Pearl/Logger.h
@@ -25,6 +25,7 @@
 #import <Foundation/Foundation.h>
 #import <libgen.h>
 
+#define trc(format, ...)    [[Logger get] trc:@"%25s:%-3d | " format, basename(__FILE__), __LINE__ , ##__VA_ARGS__]
 #define dbg(format, ...)    [[Logger get] dbg:@"%25s:%-3d | " format, basename(__FILE__), __LINE__ , ##__VA_ARGS__]
 #define inf(format, ...)    [[Logger get] inf:@"%25s:%-3d | " format, basename(__FILE__), __LINE__ , ##__VA_ARGS__]
 #define wrn(format, ...)    [[Logger get] wrn:@"%25s:%-3d | " format, basename(__FILE__), __LINE__ , ##__VA_ARGS__]
@@ -73,6 +74,8 @@ typedef enum LogLevel {
 - (Logger *)logWithLevel:(LogLevel)aLevel andMessage:(NSString *)format, ...;
 /** Print all log messages of the given level or above to the console. */
 - (void)printAllWithLevel:(LogLevel)level;
+/** Log a new TRACE-level event. */
+- (Logger *)trc:(NSString *)format, ...;
 /** Log a new DEBUG-level event. */
 - (Logger *)dbg:(NSString *)format, ...;
 /** Log a new INFO-level event. */

Pearl/Logger.m
@@ -181,6 +181,17 @@ static NSDateFormatter *logDateFormatter = nil;
 }
 
 
+- (Logger *)trc:(NSString *)format, ... {
+    
+    va_list argList;
+    va_start(argList, format);
+    NSString *message = [[NSString alloc] initWithFormat:format arguments:argList];
+    va_end(argList);
+    
+    return [self logWithLevel:LogLevelTrace andMessage:[message autorelease]];
+}
+
+
 - (Logger *)dbg:(NSString *)format, ... {
 
     va_list argList;
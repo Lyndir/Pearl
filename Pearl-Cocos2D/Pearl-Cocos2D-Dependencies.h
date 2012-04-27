52ed34d12afd56e40725aeda57607864d8012c65
Maarten Billemont <lhunath@lyndir.com>
2012-04-27 12:21:08 +0200

    Re-license as LGPLv3

--- Pearl-Cocos2D/Pearl-Cocos2D-Dependencies.h
+++ Pearl-Cocos2D/Pearl-Cocos2D-Dependencies.h
@@ -1,2 +1,11 @@
+/**
+ * Copyright Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
+ *
+ * See the enclosed file LICENSE for license information (LGPLv3). If you did
+ * not receive this file, see http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * @author   Maarten Billemont <lhunath@lyndir.com>
+ * @license  http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 #import "cocos2d.h"
 #import "Pearl-UIKit.h"
--- Pearl-Cocos2D/View/Menus/PearlCCMenuItemSymbolic.m
+++ Pearl-Cocos2D/View/Menus/PearlCCMenuItemSymbolic.m
@@ -1,3 +1,12 @@
+/**
+ * Copyright Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
+ *
+ * See the enclosed file LICENSE for license information (LGPLv3). If you did
+ * not receive this file, see http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * @author   Maarten Billemont <lhunath@lyndir.com>
+ * @license  http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 //
 //  PearlCCMenuItemSymbolic.m
 //  Pearl

204b1a82b4101c337fdb8a1d31a76da387716ce9
Maarten Billemont <lhunath@lyndir.com>
2012-05-15 14:55:01 +0200

    Move ARC check into .m file so it doesn't require the main application to be ARC enabled.

--- Pearl-UIKit/Pearl-UIKit.h
+++ Pearl-UIKit/Pearl-UIKit.h
@@ -2,10 +2,6 @@
 #error PEARL_UIKIT used but not enabled.  If you want to use this library, first enable it with #define PEARL_UIKIT in your Pearl prefix file.
 #endif
 
-#if ! __has_feature(objc_arc)
-#error PEARL_UIKIT requires ARC.  Change your build settings to enable ARC support in your compiler and try again.
-#endif
-
 #import "Pearl-UIKit-Dependencies.h"
 #import "Pearl.h"
 #import "PearlAlert.h"

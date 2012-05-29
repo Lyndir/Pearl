97095bda47a5e2c610160612bc453b6280a88eed
Maarten Billemont <lhunath@lyndir.com>
2012-05-29 11:06:57 +0200

    A queue implementation.
    [ADDED]     Category on NSMutableArray to make it function like a queue.

--- Pearl/Pearl.h
+++ Pearl/Pearl.h
@@ -2,6 +2,7 @@
 #error PEARL used but not enabled.  If you want to use this library, first enable it with #define PEARL in your Pearl prefix file.
 #endif
 
+#import "NSMutableArray_PearlQueue.h"
 #import "NSObject_PearlExport.h"
 #import "NSString_PearlNSArrayFormat.h"
 #import "NSString_PearlSEL.h"

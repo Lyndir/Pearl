73acf0f3cba50decb54be13fc7f8a1a312e2cd6d
Maarten Billemont <lhunath@lyndir.com>
2014-09-02 21:39:55 -0400

    Resolve a situation when a custom supportedInterfaceOrientations doesn't match with the preferredInterfaceOrientationForPresentation.

--- Pearl-UIKit/PearlNavigationController.m
+++ Pearl-UIKit/PearlNavigationController.m
@@ -1,12 +1,12 @@
 /**
- * Copyright Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
- *
- * See the enclosed file LICENSE for license information (LGPLv3). If you did
- * not receive this file, see http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * @author   Maarten Billemont <lhunath@lyndir.com>
- * @license  http://www.gnu.org/licenses/lgpl-3.0.txt
- */
+* Copyright Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
+*
+* See the enclosed file LICENSE for license information (LGPLv3). If you did
+* not receive this file, see http://www.gnu.org/licenses/lgpl-3.0.txt
+*
+* @author   Maarten Billemont <lhunath@lyndir.com>
+* @license  http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
 
 //
 //  PearlNavigationController.h
@@ -19,81 +19,86 @@
 #import "PearlNavigationController.h"
 
 @implementation PearlNavigationController {
-}
-
-- (UINavigationController *)initWithNavigationBarClass:(Class)navigationBarClass toolbarClass:(Class)toolbarClass {
-
-    if (!(self = [super initWithNavigationBarClass:navigationBarClass toolbarClass:toolbarClass]))
-        return nil;
-
-    [self initDefaults];
-
-    return self;
-}
-
-- (id)initWithRootViewController:(UIViewController *)rootViewController {
-
-    if (!(self = [super initWithRootViewController:rootViewController]))
-        return nil;
-
-    [self initDefaults];
-
-    return self;
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-
-    if (!(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
-        return nil;
-
-    [self initDefaults];
-
-    return self;
-}
-
-- (id)initWithCoder:(NSCoder *)coder {
-
-    if (!(self = [super initWithCoder:coder]))
-        return nil;
-
-    [self initDefaults];
-
-    return self;
-}
-
-- (id)init {
-
-    if (!(self = [super init]))
-        return nil;
-
-    [self initDefaults];
-
-    return self;
-}
-
-- (void)initDefaults {
-
-    self.shouldAutorotate = [super shouldAutorotate];
-    self.supportedInterfaceOrientations = [super supportedInterfaceOrientations];
-    self.preferredInterfaceOrientationForPresentation = [super preferredInterfaceOrientationForPresentation];
+    NSNumber *_shouldAutorotate;
+    NSNumber *_supportedInterfaceOrientations;
+    NSNumber *_preferredInterfaceOrientationForPresentation;
 }
 
 - (BOOL)shouldAutorotate {
 
-    return self.forwardInterfaceRotation && self.topViewController?
-           [self.topViewController shouldAutorotate]: _shouldAutorotate;
+    if (self.forwardInterfaceRotation && self.topViewController)
+        return [self.topViewController shouldAutorotate];
+
+    if (_shouldAutorotate)
+        return [_shouldAutorotate boolValue];
+
+    return [super shouldAutorotate];
 }
 
-- (NSUInteger)supportedInterfaceOrientations {
+- (void)setShouldAutorotate:(BOOL)shouldAutorotate {
 
-    return self.forwardInterfaceRotation && self.topViewController?
-           [self.topViewController supportedInterfaceOrientations]: _supportedInterfaceOrientations;
+    _shouldAutorotate = @(shouldAutorotate);
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+
+    if (self.forwardInterfaceRotation && self.topViewController)
+        return [self.topViewController supportedInterfaceOrientations];
+
+    if (_supportedInterfaceOrientations)
+        return [_supportedInterfaceOrientations unsignedIntegerValue];
+
+    return [super supportedInterfaceOrientations];
+}
+
+- (void)setSupportedInterfaceOrientations:(UIInterfaceOrientationMask)supportedInterfaceOrientations {
+
+    _supportedInterfaceOrientations = @(supportedInterfaceOrientations);
 }
 
 - (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
 
-    return self.forwardInterfaceRotation && self.topViewController?
-           [self.topViewController preferredInterfaceOrientationForPresentation]: _preferredInterfaceOrientationForPresentation;
+    UIInterfaceOrientation preferredInterfaceOrientationForPresentation = 0;
+    UIInterfaceOrientationMask supportedInterfaceOrientations = [self supportedInterfaceOrientations];
+
+    if (self.forwardInterfaceRotation && self.topViewController) {
+        if (1 << (preferredInterfaceOrientationForPresentation = [self.topViewController preferredInterfaceOrientationForPresentation]) &
+            supportedInterfaceOrientations)
+            return preferredInterfaceOrientationForPresentation;
+    }
+
+    if (_preferredInterfaceOrientationForPresentation) {
+        if (1 << (preferredInterfaceOrientationForPresentation = [_preferredInterfaceOrientationForPresentation integerValue]) &
+            supportedInterfaceOrientations)
+            return preferredInterfaceOrientationForPresentation;
+    }
+
+    if (1 << (preferredInterfaceOrientationForPresentation = [super preferredInterfaceOrientationForPresentation]) &
+        supportedInterfaceOrientations)
+        return preferredInterfaceOrientationForPresentation;
+
+    if (1 << (preferredInterfaceOrientationForPresentation = UIInterfaceOrientationPortrait) &
+        supportedInterfaceOrientations)
+        return preferredInterfaceOrientationForPresentation;
+
+    if (1 << (preferredInterfaceOrientationForPresentation = UIInterfaceOrientationLandscapeLeft) &
+        supportedInterfaceOrientations)
+        return preferredInterfaceOrientationForPresentation;
+
+    if (1 << (preferredInterfaceOrientationForPresentation = UIInterfaceOrientationLandscapeRight) &
+        supportedInterfaceOrientations)
+        return preferredInterfaceOrientationForPresentation;
+
+    if (1 << (preferredInterfaceOrientationForPresentation = UIInterfaceOrientationPortraitUpsideDown) &
+        supportedInterfaceOrientations)
+        return preferredInterfaceOrientationForPresentation;
+
+    Throw( @"No preferred orientation supports the supported orientations: %d", supportedInterfaceOrientations );
+}
+
+- (void)setPreferredInterfaceOrientationForPresentation:(UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+
+    _preferredInterfaceOrientationForPresentation = @(preferredInterfaceOrientationForPresentation);
 }
 
 @end

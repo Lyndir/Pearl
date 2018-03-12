4421a1f5b1211253244e65104a06cc5aaf0d4db5
Maarten Billemont <lhunath@lyndir.com>
2018-03-11 23:25:45 -0400

    Utility for installing a layout guide that automatically updates to the keyboard space when it shows.

---
+++ Pearl-UIKit/UILayoutGuide+Pearl.m
@@ -0,0 +1,55 @@
+//
+// Created by Maarten Billemont on 2018-03-11.
+// Copyright (c) 2018 Lyndir. All rights reserved.
+//
+
+#import "UILayoutGuide+Pearl.h"
+
+@implementation UILayoutGuide(Pearl)
+
++ (instancetype)installKeyboardLayoutGuideInView:(UIView *)view
+                                     constraints:(NSArray <NSLayoutConstraint *> *( ^ )(UILayoutGuide *keyboardLayoutGuide))block {
+
+    UILayoutGuide *layoutGuide = [self new];
+    [view addLayoutGuide:layoutGuide];
+
+    NSLayoutConstraint *keyboardTopConstraint = [[layoutGuide.topAnchor constraintEqualToAnchor:view.topAnchor] activate];
+    NSLayoutConstraint *keyboardLeftConstraint = [[layoutGuide.leftAnchor constraintEqualToAnchor:view.leftAnchor] activate];
+    NSLayoutConstraint *keyboardRightConstraint = [[layoutGuide.rightAnchor constraintEqualToAnchor:view.rightAnchor] activate];
+    NSLayoutConstraint *keyboardBottomConstraint = [[layoutGuide.bottomAnchor constraintEqualToAnchor:view.bottomAnchor] activate];
+    NSArray *layoutConstraints = block( layoutGuide );
+
+    PearlAddNotificationObserverTo( layoutGuide, UIKeyboardWillChangeFrameNotification, nil, NSOperationQueue.mainQueue,
+            ^(UILayoutGuide *layoutGuide, NSNotification *notification) {
+                CGRect keyboardScreenFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+                CGRect keyboardFrame = [view convertRect:keyboardScreenFrame fromCoordinateSpace:[UIScreen mainScreen].coordinateSpace];
+                keyboardTopConstraint.constant = CGRectGetMinY( keyboardFrame );
+                keyboardLeftConstraint.constant = CGRectGetMinX( keyboardFrame );
+                keyboardRightConstraint.constant = CGRectGetMaxX( keyboardFrame ) - CGRectGetMaxX( view.bounds );
+                keyboardBottomConstraint.constant = CGRectGetMaxY( keyboardFrame ) - CGRectGetMaxY( view.bounds );
+            } );
+    PearlAddNotificationObserverTo( layoutGuide, UIKeyboardWillShowNotification, nil, NSOperationQueue.mainQueue,
+            ^(UILayoutGuide *layoutGuide, NSNotification *notification) {
+                NSTimeInterval duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]?: 0.3;
+                [view layoutIfNeeded];
+                [UIView animateWithDuration:duration animations:^{
+                    for (NSLayoutConstraint *constraint in layoutConstraints)
+                        [constraint activate];
+                    [view layoutIfNeeded];
+                }];
+            } );
+    PearlAddNotificationObserverTo( layoutGuide, UIKeyboardWillHideNotification, nil, NSOperationQueue.mainQueue,
+            ^(UILayoutGuide *layoutGuide, NSNotification *notification) {
+                NSTimeInterval duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]?: 0.3;
+                [view layoutIfNeeded];
+                [UIView animateWithDuration:duration animations:^{
+                    for (NSLayoutConstraint *constraint in layoutConstraints)
+                        [constraint deactivate];
+                    [view layoutIfNeeded];
+                }];
+            } );
+
+    return layoutGuide;
+}
+
+@end

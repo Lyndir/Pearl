0566d8ab3154737c5dbe9493e6ff160fb055473d
Maarten Billemont <lhunath@lyndir.com>
2019-07-04 16:49:15 -0400

    Use property syntax to fix accessors in Swift.

--- Pearl/NSInvocation+Pearl.h
+++ Pearl/NSInvocation+Pearl.h
@@ -66,21 +66,21 @@ extern NSValue *PearlSwizzleIMP(Class type, SEL sel, IMP original, id block);
 
 @interface NSValue(Pearl)
 
-- (char)charValue;
-- (unsigned char)unsignedCharValue;
-- (short)shortValue;
-- (unsigned short)unsignedShortValue;
-- (int)intValue;
-- (unsigned int)unsignedIntValue;
-- (long)longValue;
-- (unsigned long)unsignedLongValue;
-- (long long)longLongValue;
-- (unsigned long long)unsignedLongLongValue;
-- (float)floatValue;
-- (double)doubleValue;
-- (BOOL)boolValue;
-- (NSInteger)integerValue;
-- (NSUInteger)unsignedIntegerValue;
-- (NSString *)stringValue;
+@property(readonly) char charValue;
+@property(readonly) unsigned char unsignedCharValue;
+@property(readonly) short shortValue;
+@property(readonly) unsigned short unsignedShortValue;
+@property(readonly) int intValue;
+@property(readonly) unsigned int unsignedIntValue;
+@property(readonly) long longValue;
+@property(readonly) unsigned long unsignedLongValue;
+@property(readonly) long long longLongValue;
+@property(readonly) unsigned long long unsignedLongLongValue;
+@property(readonly) float floatValue;
+@property(readonly) double doubleValue;
+@property(readonly) BOOL boolValue;
+@property(readonly) NSInteger integerValue;
+@property(readonly) NSUInteger unsignedIntegerValue;
+@property(readonly) NSString *stringValue;
 
 @end
